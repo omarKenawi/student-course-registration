1f7aff67a4b4cca7b6eab6e3159681a29983c0aa
omar <omar.sseeddeekk@gmail.com>
2026-07-26 23:22:18 +0300

    init the project

---
+++ src/test/java/com/example/studentcourseregistration/StudentCourseRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentcourseregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
